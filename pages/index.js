be39461877f4c9da8680f64c47670c4bdbbe0c81
AnnMarie Conway <aconway2@meredith.com>
2021-09-14 18:47:15 -0400

    refactor

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import appConfig from 'app_config'
 import navigationComponent from 'components/navigation-component'
 import footerComponent from 'components/footer-component'
 
-export default function Index(props) {
-  const homeComponent = () => <h1>home</h1>
+const homeComponent = () => <h1>home</h1>
 
+export default function Index(props) {
   return <>
     {navigationComponent.bind(props)()}
     {homeComponent.bind(props)()}
