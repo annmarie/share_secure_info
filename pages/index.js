f9a85221a6eeadca1670b4f4eb5847731bcd5a10
AnnMarie Conway <aconway2@meredith.com>
2021-09-15 08:28:31 -0400

    refacted page component

--- pages/index.js
+++ pages/index.js
@@ -7,11 +7,6 @@ import NavigationComponent from 'components/navigation-component';
 import FooterComponent from 'components/footer-component';
 import HomeComponent from "components/home-component";
 
-const PageComponent = (props) => {
-  return (props.pagePath == '/test') ? 
-    <TestComponent { ...props }/> : <HomeComponent { ...props } />
-}
-
 export default function Index(props) {
   const router = useRouter();
   const [pagePath, setPagePath] = useState(_.get(router, 'asPath', ''))
@@ -30,6 +25,19 @@ export async function getServerSideProps(ctx) {
   navPaths.push('/_next/data/development/index.json')
   const validUrl = navPaths.includes(reqPath) ? true : false
   if (!validUrl) return { notFound: true }
-  // assign page props
+  // pass config data to page props
   return { props: { ...appConfig } }
+}
+
+function PageComponent(props) {
+  const path = _.get(props, 'path')
+
+  switch(path) {
+    case '/':
+      return <HomeComponent { ...props } />
+    case '/test':
+      return <TestComponent { ...props } />
+    default:
+      return ''
+  }
 }
