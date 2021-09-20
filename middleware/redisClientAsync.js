71f3b687ca257e15f3f86ad97268e489abd8cab8
Rakesh Malik <rmalik@meredith.com>
2021-09-20 12:10:12 -0700

    Redis client schema update

--- middleware/redisClientAsync.js
+++ middleware/redisClientAsync.js
@@ -2,6 +2,8 @@ const redis = require("redis");
 const { promisify } = require("util");
 const logger = require("./logger");
 const config = require("./config");
+const { random } = require("lodash");
+const { randomUUID } = require("crypto");
 
 const redisClient = redis.createClient(config.redisPort, config.redisHost);
 const redisKeyPrefix = "share-secure-info";
@@ -13,10 +15,12 @@ redisClient.on("err", err => {
 const redisGetAsync = promisify(redisClient.get).bind(redisClient);
 const redisSetAsync = promisify(redisClient.setex).bind(redisClient);
 
-const asyncSetValue = (id, value) => {
-  const key = `${redisKeyPrefix}-${id}`; 
+const asyncSetValue = (value, expiration) => {
+  const id = randomUUID;
+  const key = `${redisKeyPrefix}-${id}`;
 
-  redisSetAsync(key, config.cacheExpiration, value);
+  redisSetAsync(key, expiration, value);
+  return key;
 };
 
 const asyncGetValue = id => {
