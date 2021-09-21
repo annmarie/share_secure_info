539f722c4204accc05dea7323d98dc33d51714ec
Rakesh Malik <rmalik@meredith.com>
2021-09-21 15:22:27 -0700

    Refactored for more modularity

--- pages/api/test-redis.js
+++ pages/api/test-redis.js
@@ -1,47 +1,11 @@
 import apiPageHandler from 'middleware/api-page-handler'
+import {getValue, setValue } from '../../utils/redisApi'
 import _ from 'lodash'
-import redis from 'redis'
-import * as uuid from 'uuid';
-
-// TODO: move this to it's own file under utils
-const connectRedis = () => {
-  const host = process.env.REDIS_HOST || 'localhost'
-  const port = process.env.REDIS_PORT || '6379'
-  const password = process.env.REDIS_PASSWORD
-  const config = (password) ? { host, port, password } : { host, port }
-  const client = redis.createClient(config)
-  return client.on('connect', function () {
-    console.log(`redis connected ${host}:${port}`)
-  })
-}
-const redisClient = connectRedis()
-const redisKeyPrefix = 'MYREDISKEY'
-
-const setValue = async (value) => {
-  const id = uuid.v4()
-  const key = `${redisKeyPrefix}-${id}`;
-  const doc = {
-    value: value,
-    status: 'Current'
-  }
-  redisClient.setex(key, 123332, JSON.stringify(doc))
-  return key
-}
-
-const getValue = async (id) => {
-  return new Promise((resolve, reject) => {
-    redisClient.get(id, (err, resp) => {
-      if (err) reject(err);
-
-      resolve(resp)
-    })
-  })
-}
 
 async function pageRender(_req, res) {
   const output = { status: "start" }
   // save stuff to redis
-  const setId = await setValue('some stuff i saved in redis')
+  const setId = await setValue('refactorized')
 
   const data = await getValue(setId)
 
---
+++ utils/redisApi.js
@@ -0,0 +1,26 @@
+import redisClient from './redisConnect'
+import * as uuid from 'uuid';
+import _ from 'lodash'
+
+const redisKeyPrefix = 'MYREDISKEY'
+
+export async function setValue (value)  {
+  const id = uuid.v4()
+  const key = `${redisKeyPrefix}-${id}`;
+  const doc = {
+    value: value,
+    status: 'Current'
+  }
+  redisClient.setex(key, 123332, JSON.stringify(doc))
+  return key
+}
+
+export async function getValue (id)  {
+  return new Promise((resolve, reject) => {
+    redisClient.get(id, (err, resp) => {
+      if (err) reject(err);
+
+      resolve(resp)
+    })
+  })
+}
