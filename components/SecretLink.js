86cbd5060c9f7b4edfb2e8cfd6eaafbfae3314ea
Meghal Modi <meghal.modi@temple.edu>
2021-09-22 15:48:38 -0400

    Updated button text

--- components/SecretLink.js
+++ components/SecretLink.js
@@ -48,7 +48,7 @@ export default function SecretLink (props) {
                 variant="contained"
                 onClick={handleButtonClick} 
             >
-                Copy to clipboard
+                Copy message to clipboard
             </Button>
         </Container>
     );
