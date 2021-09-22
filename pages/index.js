a4fbfd4ea89c0abf7821e54a974fc885e2ddf759
AnnMarie Conway <aconway2@meredith.com>
2021-09-22 18:46:15 -0400

    refactor

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react'
-import { useRouter } from "next/router"
 import Head from 'next/head'
 import _ from 'lodash'
 import appPageHandler from 'middleware/app-page-handler'
-import NavigationComponent from 'components/navigation-component'
 import FooterComponent from 'components/footer-component'
 import StepsComponent from 'components/steps-component'
 import HomeComponent from "components/home-component"
@@ -25,45 +23,16 @@ export default function Index(props) {
       <title>Share Secure Info</title>
     </Head>
 
-    <NavigationComponent { ...props } />
     <HomeComponent { ...props } />
     <StepsComponent { ...props} />
     <FooterComponent { ...props } /> 
   </>
 }
 
-function PageComponent(props) {
-  const router = useRouter()
-  const path = _.get(router, 'asPath', '/')
- 
-  switch(path) {
-    case '/':
-      return <HomeComponent { ...props } />
-    case '/test':
-      return <TestComponent { ...props } />
-    default:
-      return ''
-  }
-}
-
 // using this instead of `getInitialProps`
 export function getServerSideProps(ctx) {
   // middleware
   appPageHandler(ctx.req, ctx.res)
-
-  // validate request url against the list of nav links from the config
-  const [reqPath, _qs] = _.get(ctx, 'req.url', '').split('?')
-  const appConfig = ctx.req.appConfig
-  const navPaths = appConfig.navLinks.map(navLink => navLink.path)
-  // if the user clicked a LINK component
-  if (reqPath.match(/\/_next\/data\/.*\/index.json/)) {
-    // add the url as valid 
-    navPaths.push(reqPath)
-  }
-  const validUrl = navPaths.includes(reqPath) ? true : false
-
-  // if no valid url path is found render 404 page
-  if (!validUrl) return { notFound: true }
 
   // pass config data to page props
   return { props: { ...appConfig } }
