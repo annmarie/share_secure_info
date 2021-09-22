1b013010351ab1599f9b07807297c6e4e8f3378b
AnnMarie Conway <aconway2@meredith.com>
2021-09-22 18:49:13 -0400

    refactor

--- app-config.js
+++ app-config.js
@@ -1,11 +1,9 @@
 
 const appConfig = {
-    useNavigation: false,
     useFooter: false,
     footerText: 'This is the footer',
     navLinks: [
         { id: 1, title: 'home', path: '/' },
-        { id: 2, title: 'test', path: '/test' },
     ],
 }
 
