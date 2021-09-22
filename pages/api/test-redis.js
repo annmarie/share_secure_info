7e40879a81970a657ffa472e9377d02cb2140fb7
AnnMarie Conway <aconway2@meredith.com>
2021-09-22 16:22:03 -0400

    minor refactor

--- pages/api/test-redis.js
+++ pages/api/test-redis.js
@@ -3,8 +3,8 @@ import {getValue, setValue } from '../../utils/redisApi'
 import _ from 'lodash'
 
 
-async function requestHandler(_req, res) {
-  if(_req.method === 'POST'){
+async function requestHandler(req, res) {
+  if(req.method === 'POST'){
   const output = { status: "start" }
   // save stuff to redis
   const setId = await setValue('refactorized')
@@ -15,7 +15,7 @@ async function requestHandler(_req, res) {
   _.set(output, 'data', data)
   // render page
   res.status(200).json(output)
-  } else if(_req.method === 'GET') {
+  } else if(req.method === 'GET') {
 
     // retrieve link goes here
     res.status(200).json({msg:false})
