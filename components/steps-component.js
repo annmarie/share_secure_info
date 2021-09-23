31eead5bcf84b9fcf107ceb53fd2b29244758295
Emilia Anderson <emiliaa@allrecipes.com>
2021-09-23 09:29:45 -0700

    instructions update

--- components/steps-component.js
+++ components/steps-component.js
@@ -12,17 +12,20 @@ export default function StepsComponent(props) {
       <ul className={styles.steps}>
         <li>
             <h3>Instructions <span className={styles.lightItalic}>optional</span></h3>
-            <p>If your secret needs an explanation of how to use it, when to use it, etc, you can add here and it will be sent along with the sensitive info you are sharing.</p>
+            <p>If your secret needs an explanation of how to use it, when to use it, etc, you can add here and it will be sent along with the sensitive info you are sharing, and will be included in the encryption of the secret.</p>
         </li>
         <li>
           <h3>Secret</h3>
-          <p>The field to add your sensitive information.</p>
+          <p>The field to add your sensitive information. If needed, instructions for use (see #1) can be included, and will be encrypted along with the secret.</p>
         </li>
         <li>
           <h3>Valid Until</h3>
           <p>In this field, you can set how long you would like the link to the sensitive info to remain active.  This can be set in minutes, hours or days.</p>
         </li>
       </ul>
+      <h3>The Encrypted Link</h3>
+          <p>After submitting, a link will be created and presented on the page, with a note as to how long the link will be valid for, and an explanation of how long you will have to copy the secret once you visit the link.</p>
+          <p>If needed, you can copy the whole message to the clipboard to save for later, or, you can just copy the link itself.</p>
     </div>
   </>
 }
