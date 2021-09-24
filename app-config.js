49de70afce6ad9b66da72580678d68a64370dc9d
AnnMarie Conway <aconway2@meredith.com>
2021-09-23 22:22:47 -0400

    more cleanup

--- app-config.js
+++ app-config.js
@@ -2,10 +2,7 @@
 const appConfig = {
     useHeader: true,
     useFooter: true,
-    footerText: 'This is the footer',
-    navLinks: [
-        { id: 1, title: 'home', path: '/' },
-    ],
+    navLinks: [ { id: 1, title: 'home', path: '/' } ],
     teamMembers: [
         'Abby Sobolevsky',
         'Alejandro Hoyos',
