651069d5bf2825dcbd9e410c411761ae45851d5c
AnnMarie Conway <aconway2@meredith.com>
2021-09-26 19:40:02 -0400

    removed themes because it breaks in prod :(

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import Head from "next/head";
-import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import HeaderComponent from "components/header-component";
 import FooterComponent from "components/footer-component";
 import "styles/globals.scss";
@@ -15,25 +14,15 @@ function ShareSecureApp({ Component, pageProps }) {
     }
   }, []);
 
-  const theme = createTheme({
-    palette: {
-      primary: {
-        main: "#673ab7"
-      }
-    }
-  });
-
   return (
     <>
       <Head>
         <title>ShareSecure</title>
       </Head>
 
-      <MuiThemeProvider theme={theme}>
-        <HeaderComponent {...pageProps} />
-        <Component {...pageProps} />
-        <FooterComponent {...pageProps} />
-      </MuiThemeProvider>
+      <HeaderComponent {...pageProps} />
+      <Component {...pageProps} />
+      <FooterComponent {...pageProps} />
     </>
   );
 }
