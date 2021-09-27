983cc2a11a28bd031d0cba9b468e0b3adffdcda5
AnnMarie Conway <aconway2@meredith.com>
2021-09-26 23:05:00 -0400

    theme/document working

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,30 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/styles";
 
 class ShareSecureDocument extends Document {
   static async getInitialProps(ctx) {
-    const initProps = await Document.getInitialProps(ctx);
-    return { ...initProps };
+    const sheet = new ServerStyleSheets();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) => sheet.collect(<App {...props} />)
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        )
+      };
+    } finally {
+      ctx.renderPage(sheet);
+    }
   }
 
   componentDidMount() {
