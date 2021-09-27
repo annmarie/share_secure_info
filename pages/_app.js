65e737a71873cbbf805d28c58a7bc3280dfac792
AnnMarie Conway <aconway2@meredith.com>
2021-09-26 21:58:55 -0400

    added babel-plugin-styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import "styles/globals.scss";
 import HeaderComponent from "components/header-component";
 import FooterComponent from "components/footer-component";
+//import Layout from "components/Layout";
+//<Layout {...pageProps}></Layout>
 
 function ShareSecureApp({ Component, pageProps }) {
   return (
