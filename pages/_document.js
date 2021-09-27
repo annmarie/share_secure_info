b49b3fe3e4c1ce2c34abac81ace081321e9f1c3e
AnnMarie Conway <aconway2@meredith.com>
2021-09-26 23:06:59 -0400

    lint fixes

--- pages/_document.js
+++ pages/_document.js
@@ -1,20 +1,18 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
-import { ServerStyleSheet } from 'styled-components'
-
+import { ServerStyleSheet } from "styled-components";
 
 class ShareSecureDocument extends Document {
   static async getInitialProps(ctx) {
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () =>
         originalRenderPage({
-          enhanceApp: (App) => (props) =>
-            sheet.collectStyles(<App {...props} />),
-        })
+          enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />)
+        });
 
-      const initialProps = await Document.getInitialProps(ctx)
+      const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
         styles: (
@@ -22,10 +20,10 @@ class ShareSecureDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        ),
-      }
+        )
+      };
     } finally {
-      sheet.seal()
+      sheet.seal();
     }
   }
 
